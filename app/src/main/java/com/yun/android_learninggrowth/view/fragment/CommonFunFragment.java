dc671179e4a5727166f78030df249770c4867bbd
yeyunfei <yf.ye@skysoultions.cn>
2017-03-24 08:50:27 +0800

    手机验证码ContDownTimer,自定义CountTimers

--- app/src/main/java/com/yun/android_learninggrowth/view/fragment/CommonFunFragment.java
+++ app/src/main/java/com/yun/android_learninggrowth/view/fragment/CommonFunFragment.java
@@ -1,13 +1,9 @@
 package com.yun.android_learninggrowth.view.fragment;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
